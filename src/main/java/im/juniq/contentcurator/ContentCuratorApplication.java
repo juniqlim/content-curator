6105b50187ea1dec6afd39f7e60726cbe48312f2
juniq <juniqlim@gmail.com>
2021-06-26 15:47:24 +0900

    init

---
+++ src/main/java/im/juniq/contentcurator/ContentCuratorApplication.java
@@ -0,0 +1,13 @@
+package im.juniq.contentcurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCuratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentCuratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/im/juniq/contentcurator/ContentCuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package im.juniq.contentcurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCuratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
